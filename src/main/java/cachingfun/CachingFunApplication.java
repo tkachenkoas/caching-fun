2293ae014a8b8a914a9479128601c508fe54f252
Anton Tkachenko <anton.tkachenko@duda.co>
2022-01-12 21:43:12 +0200

    init

---
+++ src/main/java/cachingfun/CachingFunApplication.java
@@ -0,0 +1,13 @@
+package cachingfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachingFunApplication.class);
+    }
+
+}
